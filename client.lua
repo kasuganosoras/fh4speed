6e8bd7bdf1879299b8043c3cabc2e280226f29d8
Akkariin Meiko <akkariins@gmail.com>
2021-02-06 10:05:21 +0800

    Update client.lua

--- client.lua
+++ client.lua
@@ -4,60 +4,83 @@
 
 local isHide = false
 
+local carRPM, carSpeed, carGear, carIL, carAcceleration, carHandbrake, carBrakeABS, carLS_r, carLS_o, carLS_h
+
+RegisterCommand("fh4speed", function(_, args)	
+	ToggleDisplay()
+end, false)
+
+RegisterKeyMapping('fh4speed', 'Enable or disable speedometer', 'keyboard', 'f10')
+
 Citizen.CreateThread(function()
 	while true do
-		Wait(1)
+		Wait(50)
+
 		playerPed = GetPlayerPed(-1)
-		if IsPedInAnyVehicle(playerPed) then
 		
-			if playerPed and not isHide then
+		if playerPed and IsPedInAnyVehicle(playerPed) and not isHide then
+			
+			playerCar = GetVehiclePedIsIn(playerPed, false)
+			
+			if playerCar and GetPedInVehicleSeat(playerCar, -1) == playerPed then
+				local NcarRPM                      = GetVehicleCurrentRpm(playerCar)
+				local NcarSpeed                    = GetEntitySpeed(playerCar)
+				local NcarGear                     = GetVehicleCurrentGear(playerCar)
+				local NcarIL                       = GetVehicleIndicatorLights(playerCar)
+				local NcarAcceleration             = IsControlPressed(0, 71)
+				local NcarHandbrake                = GetVehicleHandbrake(playerCar)
+				local NcarBrakeABS                 = (GetVehicleWheelSpeed(playerCar, 0) <= 0.0) and (NcarSpeed > 0.0)
+				local NcarLS_r, NcarLS_o, NcarLS_h = GetVehicleLightsState(playerCar)
 				
-				playerCar = GetVehiclePedIsIn(playerPed, false)
+				local shouldUpdate = false
 				
-				if playerCar and GetPedInVehicleSeat(playerCar, -1) == playerPed then
-					
-					carRPM                    = GetVehicleCurrentRpm(playerCar)
-					carSpeed                  = GetEntitySpeed(playerCar)
-					carGear                   = GetVehicleCurrentGear(playerCar)
-					carIL                     = GetVehicleIndicatorLights(playerCar)
-					carHandbrake              = GetVehicleHandbrake(playerCar)
-					carBrakePressure          = GetVehicleWheelBrakePressure(playerCar, 0)
-					carLS_r, carLS_o, carLS_h = GetVehicleLightsState(playerCar)
-					
-					SendNUIMessage({
-						ShowHud             = true,
-						CurrentCarRPM       = carRPM,
-						CurrentCarGear      = carGear,
-						CurrentCarSpeed     = carSpeed,
-						CurrentCarKmh       = math.ceil(carSpeed * 3.6),
-						CurrentCarMph       = math.ceil(carSpeed * 2.236936),
-						CurrentCarIL        = carIL,
-						CurrentCarHandbrake = carHandbrake,
-						CurrentCarBrake     = carBrakePressure,
-						CurrentCarLS_r      = carLS_r,
-						CurrentCarLS_o      = carLS_o,
-						CurrentCarLS_h      = carLS_h,
-						PlayerID            = GetPlayerServerId(GetPlayerIndex())
-					})
-					
-				else
-					SendNUIMessage({HideHud = true})
+				if NcarRPM ~= carRPM or NcarSpeed ~= carSpeed or NcarGear ~= carGear or NcarIL ~= carIL or NcarAcceleration ~= carAcceleration 
+					or NcarHandbrake ~= carHandbrake or NcarBrakeABS ~= carBrakeABS or NcarLS_r ~= carLS_r or NcarLS_o ~= carLS_o or NcarLS_h ~= carLS_h then
+					shouldUpdate = true
 				end
+				
+				if shouldUpdate then
+					carRPM          = NcarRPM
+					carGear         = NcarGear
+					carSpeed        = NcarSpeed
+					carIL           = NcarIL
+					carAcceleration = NcarAcceleration
+					carHandbrake    = NcarHandbrake
+					carBrakeABS     = NcarBrakeABS
+					carLS_r         = NcarLS_r
+					carLS_o         = NcarLS_o
+					carLS_h         = NcarLS_h
+					SendNUIMessage({
+						ShowHud                = true,
+						CurrentCarRPM          = carRPM,
+						CurrentCarGear         = carGear,
+						CurrentCarSpeed        = carSpeed,
+						CurrentCarKmh          = math.ceil(carSpeed * 3.6),
+						CurrentCarMph          = math.ceil(carSpeed * 2.236936),
+						CurrentCarIL           = carIL,
+						CurrentCarAcceleration = carAcceleration,
+						CurrentCarHandbrake    = carHandbrake,
+						CurrentCarABS          = GetVehicleWheelBrakePressure(playerCar, 0) > 0 and not carBrakeABS,
+						CurrentCarLS_r         = carLS_r,
+						CurrentCarLS_o         = carLS_o,
+						CurrentCarLS_h         = carLS_h,
+						PlayerID               = GetPlayerServerId(GetPlayerIndex())
+					})
+				else
+					Wait(100)
+				end
+			else
+				SendNUIMessage({HideHud = true})
 			end
 		end
 	end
 end)
 
-Citizen.CreateThread(function()
-	while true do
-		Wait(0)
-		if IsControlJustPressed(1, 57) then
-			if isHide then
-				isHide = false
-			else
-				SendNUIMessage({HideHud = true})
-				isHide = true
-			end
-		end
+function ToggleDisplay()
+	if isHide then
+		isHide = false
+	else
+		SendNUIMessage({HideHud = true})
+		isHide = true
 	end
-end)
+end
