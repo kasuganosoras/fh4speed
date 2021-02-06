cf9eb3e154c887635870346e5e7639469a2dde6c
Akkariin Meiko <akkariins@gmail.com>
2021-02-06 10:06:04 +0800

    Update hud.js

--- html/js/hud.js
+++ html/js/hud.js
@@ -1,45 +1,48 @@
+var s_playerID;
+var s_Rpm = 0.0;
+var s_Speed;
+var s_Gear;
+var s_IL;
+var s_Acceleration;
+var s_Handbrake;
+var s_LS_r;
+var s_LS_o;
+var s_LS_h;
+var CalcSpeed;
+var speedText = '';
+var inVehicle = false;
+var o_rpm;
+var hasFilter = false;
+var OverLoadRPM = false;
+var IsOverLoad = false;
+
 $(function() {
-	
-	var s_playerID;
-	var s_rpm;
-	var s_speed;
-	var s_gear;
-	var s_IL;
-	var s_Handbrake;
-	var s_LS_r;
-	var s_LS_o;
-	var s_LS_h;
-	var calcSpeed;
-	var speedText = '';
-	var inVehicle = false;
 	
     window.addEventListener("message", function(event) {
         var item = event.data;
         
         if (item.ShowHud) {
 			
-			inVehicle   = true;
-			s_PlayerID  = item.PlayerID;
-			s_Rpm       = item.CurrentCarRPM;
-			s_Speed     = item.CurrentCarSpeed;
-			s_Kmh       = item.CurrentCarKmh;
-			s_Mph       = item.CurrentCarMph;
-			s_Gear      = item.CurrentCarGear;
-			s_IL        = item.CurrentCarIL;
-			s_Handbrake = item.CurrentCarHandbrake;
-			s_Brake     = item.CurrentCarBrake;
-			s_LS_r      = item.CurrentCarLS_r;
-			s_LS_o      = item.CurrentCarLS_o;
-			s_LS_h      = item.CurrentCarLS_h;
-			CalcSpeed   = s_Kmh;
-			CalcRpm     = (s_Rpm * 9);
+			inVehicle      = true;
+			s_PlayerID     = item.PlayerID;
+			s_Rpm          = item.CurrentCarRPM;
+			s_Speed        = item.CurrentCarSpeed;
+			s_Kmh          = item.CurrentCarKmh;
+			s_Mph          = item.CurrentCarMph;
+			s_Gear         = item.CurrentCarGear;
+			s_IL           = item.CurrentCarIL;
+			s_Acceleration = item.CurrentCarAcceleration;
+			s_Handbrake    = item.CurrentCarHandbrake;
+			s_ABS          = item.CurrentCarABS;
+			s_LS_r         = item.CurrentCarLS_r;
+			s_LS_o         = item.CurrentCarLS_o;
+			s_LS_h         = item.CurrentCarLS_h;
+			CalcSpeed      = s_Kmh;
+			CalcRpm        = (s_Rpm * 9);
 			
 			if(CalcSpeed > 999) {
 				CalcSpeed = 999;
 			}
-			
-			// Vehicle RPM display
-			$("#rpmshow").attr("data-value", CalcRpm.toFixed(2));
 			
 			// Vehicle Gear display
 			if(s_Gear == 0) {
@@ -52,7 +55,25 @@ $(function() {
 				} else {
 					$(".geardisplay").removeAttr("style");
 				}
+				if(CalcRpm >= 9) {
+					IsOverLoad = true;
+					if(OverLoadRPM) {
+						CalcRpm = 9;
+						s_Rpm = 9;
+						OverLoadRPM = false;
+					} else {
+						var tempRandom = Math.random();
+						CalcRpm = 8 + tempRandom;
+						s_Rpm = 8 + tempRandom;
+						OverLoadRPM = true;
+					}
+				} else {
+					IsOverLoad = false;
+				}
 			}
+			
+			// Vehicle RPM display
+			$("#rpmshow").attr("data-value", CalcRpm.toFixed(2));
 			
 			// Vehicle speed display
 			if(CalcSpeed >= 100) {
@@ -75,7 +96,7 @@ $(function() {
 			}
 			
 			// Brake ABS
-			if(s_Brake > 0) {
+			if(s_ABS) {
 				$(".abs").html("ABS");
 			} else {
 				$(".abs").html('<span class="gray">ABS</span>');
@@ -92,4 +113,39 @@ $(function() {
 			inVehicle = false;
         }
     });
+	
+	setInterval(function() {
+		if(((o_rpm != s_Rpm)) || IsOverLoad) {
+			if(o_rpm - s_Rpm > 0.1 || s_Rpm - o_rpm > 0.1 || IsOverLoad) {
+				if(!hasFilter) {
+					if(s_Rpm > o_rpm) {
+						if(s_Acceleration) {
+							$("#rpmshow").css('filter', 'url(#blur3)');
+						} else {
+							$("#rpmshow").css('filter', 'url(#blur2)');
+						}
+					} else {
+						if(s_Acceleration) {
+							$("#rpmshow").css('filter', 'url(#blur3)');
+						} else {
+							$("#rpmshow").css('filter', 'url(#blur2)');
+						}
+					}
+					// $("#rpmshow").css('filter', 'url(#blur2)');
+					hasFilter = true;
+				}
+			} else {
+				if(hasFilter) {
+					$("#rpmshow").css('filter', 'drop-shadow(0px 0px 4px rgba(0,0,0,0.1))');
+					hasFilter = false;
+				}
+			}
+			o_rpm = s_Rpm;
+		} else {
+			if(hasFilter) {
+				$("#rpmshow").css('filter', 'drop-shadow(0px 0px 4px rgba(0,0,0,0.1))');
+				hasFilter = false;
+			}
+		}
+	}, 100);
 });
